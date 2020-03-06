ca19c8886c4b8e15fbf3bafb4ed3f11b54e813a1
leighmaj <51490479+leighmaj@users.noreply.github.com>
2020-03-06 13:27:35 -0600

    completed Counting and Powers

---
+++ Counting.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class Counting
+    {
+        static void Main(string[] args)
+        {
+            //COUNTING Class: Counting.cs1.Display numbers from 1 to 25 on the screen with a loop.2.Display numbers from 25 to 1 on the screen 
+            //        with a loop.3.Display numbers from 0 to 50 by 2’s with a loop.  4.Display numbers from 1 to 10 by 0.5 with a loop.
+            //        Example Reference: All Looping classes in _5Friday.
+
+            for (int i = 0; i <= 25; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine(i);
+            }
+
+            for (int i = 25; i > 0; i--)
+            {
+                Console.WriteLine();
+                Console.WriteLine(i);
+            }
+
+            for (int i = 0; i <= 50; i+=2)
+            {
+                Console.WriteLine();
+                Console.WriteLine(i);
+            }
+
+            for (double i = 1; i < 10; i+=0.5)
+            {
+                Console.WriteLine();
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Powers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class Powers
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            long powerOf2 = 2;
+
+            for (int i = 1; i < 50; i++)
+            {
+                powerOf2 *= 2;
+                Console.WriteLine(powerOf2);
+            }
+        }
+    }
+}
