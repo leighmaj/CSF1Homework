21a53d10d875599799f6c1494b955b83dfc097a0
leighmaj <51490479+leighmaj@users.noreply.github.com>
2020-03-06 11:15:26 -0600

    Initial commit

---
+++ Variables.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class Variables
+    {
+        static void Main(string[] args)
+        {
+            int number = 4;
+            char character = "A";
+            string word = "swords";
+            double num2 = 12.25;
+            float num3 = 12568;
+            byte num4 = 5;
+
+        }
+    }
+}
